7a08b6703b603645c66beeb993653dacd9e5429b
jeffhardy9486 <1141598140@qq.com>
2018-12-03 18:04:05 +0800

    springboot 1
    springboot Ⅰ @RestController

---
+++ src/main/java/cn/itlaobing/spf/SrpingbootApplication.java
@@ -0,0 +1,12 @@
+package cn.itlaobing.spf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingbootApplication.class, args);
+    }
+}
